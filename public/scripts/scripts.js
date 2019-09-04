bd7f60b8cab944d23b9109d7f45589857c6254d8
andreyromanov <andreygorogogo@gmail.com>
2019-09-04 21:48:52 +0300

    Initial commit

---
+++ public/scripts/scripts.js
@@ -0,0 +1,23 @@
+$(window).load(function() {
+	$(".se-pre-con").delay(1000).fadeOut("slow");
+});
+
+AOS.init();
+
+function tab1() {
+	var imge;
+	imge = "img/Tablet2.png";
+	$("#pic1").attr("src", imge);
+}
+
+function tab2() {
+	var imge;
+	imge = "img/Tablet3.png";
+	$("#pic1").attr("src", imge);
+}
+
+function tab3() {
+	var imge;
+	imge = "img/Tablet4.png";
+	$("#pic1").attr("src", imge);
+}

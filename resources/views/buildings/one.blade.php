c80c43696f643b3f1654fb3a4074734288b6fec8
andreyromanov <andreygorogogo@gmail.com>
2019-10-16 23:00:45 +0300

    building one

--- resources/views/buildings/one.blade.php
+++ resources/views/buildings/one.blade.php
@@ -6,12 +6,12 @@
         <div class="col-md-12">
             <div class="card">
             @foreach($building as $build)
-                <div class="card-header">Реєсттрація об'єкту <b>{{$build->name}}</b></div>
-
+                <div class="card-header">Об'єкт <b>{{$build->name}}</b></div>
                 <div class="card-body">
                 
-                    BUILDING ONE - {{$build->building_id}}
-        
+                 Назва - {{$build->name}} <br>
+                 Бюджет - {{$build->budjet}} $<br>
+                 Дати: {{$build->start_date}} - {{$build->end_date}}
                 
                 </div>
             </div>

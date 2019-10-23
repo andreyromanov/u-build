c1814b1ff2ce2a692d29c726878debd09c09cf92
andreyromanov <andreygorogogo@gmail.com>
2019-10-23 23:03:49 +0300

    Building page layout

--- resources/views/buildings/one.blade.php
+++ resources/views/buildings/one.blade.php
@@ -1,23 +1,59 @@
 @extends('layouts.master')
 
 @section('content')
+    @foreach($building as $build)
 <div class="container mt-3">
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-            @foreach($building as $build)
-                <div class="card-header">Об'єкт <b>{{$build->name}}</b></div>
+            
+                <div class="card-header">Дані про об'єкт <b>{{$build->name}}</b>&nbsp;|&nbsp;Бюджет - <b>{{$build->budjet}} $</b>&nbsp;|&nbsp; Дати: <b>{{$build->start_date}} - {{$build->end_date}}</b></div>
                 <div class="card-body">
-                
-                 Назва - {{$build->name}} <br>
-                 Бюджет - {{$build->budjet}} $<br>
-                 Дати: {{$build->start_date}} - {{$build->end_date}}
-                
+
                 </div>
             </div>
-            @endforeach
+            
         </div>
     </div>
 </div>
 
+<div class="container mt-3">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header"><b>Управління об'ектом</b></div>
+                <div class="card-body">
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</div>
+
+<div class="container mt-3">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header"><b>Статистика</b></div>
+                <div class="card-body">
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</div>
+
+<div class="container mt-3">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header"><b>Економіка</b></div>
+                <div class="card-body">
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</div>
+@endforeach
 @endsection

6c6f532da43f09035afa9e67cddaffc4b0537750
andreyromanov <andreygorogogo@gmail.com>
2019-12-10 22:24:01 +0200

    modal heading

--- resources/views/buildings/one.blade.php
+++ resources/views/buildings/one.blade.php
@@ -286,7 +286,7 @@
   <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLongTitle">Нова задача</h5>
+        <h5 class="modal-title" id="exampleModalLongTitle">Редагувати</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
